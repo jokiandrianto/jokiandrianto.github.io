2a8e59612c35cf82fc5af938956568df9f5c5c81
jokiandrianto <88532033+jokiandrianto@users.noreply.github.com>
2021-12-29 11:57:39 +0700

    Add files via upload

---
+++ js/json.js
@@ -0,0 +1,177 @@
+    // sertifikat 1
+    var sertifikat_1 = [
+        {
+            "url_id" : "1", 
+            "judul"   : "Learn HTML Concept For Programming",
+            "perusahaan" : "Diselenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_html.jpg"
+        },
+        {
+            "url_id" : "2", 
+            "judul"   : "Full-Stack Website Designer",
+            "perusahaan" : "DiSelenggarakan oleh Buildwithangga",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/bwa_webDesigner_1.jpg"
+        },
+        {
+            "url_id" : "3", 
+            "judul"   : "Belajar Dasar Pemrograman JavaScript",
+            "perusahaan" : "DiSelenggarakan oleh Dicoding",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/dicoding_dasarJS.jpg"
+        },
+        {
+            "url_id" : "4", 
+            "judul"   : "Responsive Web Design",
+            "perusahaan" : "DiSelenggarakan oleh freeCodeCamp",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/fCC_webDesign.jpg"
+        },
+        {
+            "url_id" : "5", 
+            "judul"   : "Tingkatkan Produktifitas Dengan Microsoft Excel",
+            "perusahaan" : "DiSelenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_Excel.jpg"
+        },
+        {
+            "url_id" : "6", 
+            "judul"   : "Kuasai TOEFL PBT, Raih Score 600+",
+            "perusahaan" : "DiSelenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_toefl.jpg"
+        }
+    
+    ];
+    // akhir sertifikat 1
+    
+    // sertifikat 2
+    var sertifikat_2 = [
+        {
+            "url_id" : "7", 
+            "judul"   : "Statistics Foundation For Data Science",
+            "perusahaan" : "Diselenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_dataScience_1.jpg"
+        },
+        {
+            "url_id" : "8", 
+            "judul"   : "Programming Foundation For Data Science",
+            "perusahaan" : "DiSelenggarakan oleh Buildwithangga",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_dataScience2_1.jpg"
+        },
+        {
+            "url_id" : "9", 
+            "judul"   : "Belajar Membuat Front-End Web untuk Pemula",
+            "perusahaan" : "DiSelenggarakan oleh Dicoding",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/dicoding_frontendPemula.jpg"
+        },
+        {
+            "url_id" : "10", 
+            "judul"   : "Belajar Fundamental Front-End Web Development",
+            "perusahaan" : "DiSelenggarakan oleh Dicoding",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/dicoding_frontendSedang.jpg"
+        },
+        {
+            "url_id" : "11", 
+            "judul"   : "Mobile Programming Android & Kotlin",
+            "perusahaan" : "DiSelenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_android_1.jpg"
+        },
+        {
+            "url_id" : "12", 
+            "judul"   : "Mastering Mobile Programming Android",
+            "perusahaan" : "DiSelenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_android2_1.jpg"
+        }
+    
+    ];
+    // akhir sertifikat 2
+    
+    // sertifikat 3
+    var sertifikat_3 = [
+        {
+            "url_id" : "13", 
+            "judul"   : "Cloud Practitioner Essentials (Belajar Dasar AWS Cloud)",
+            "perusahaan" : "Diselenggarakan oleh Dicoding",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/dicoding_dasarAWS.jpg"
+        },
+        {
+            "url_id" : "14", 
+            "judul"   : "Pengenalan Artificial Inteligence Untuk Bisnis",
+            "perusahaan" : "Diselenggarakan oleh Skill Academy By Ruang Guru",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/SA_AI.jpg"
+        },
+        {
+            "url_id" : "15", 
+            "judul"   : "How to Become Developer From Zero to Hero",
+            "perusahaan" : "DiSelenggarakan oleh DuniaCoding",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/duniaCoding_it_1.jpg"
+        },
+        {
+            "url_id" : "16", 
+            "judul"   : "Seminar Nasional Informasi dan Teknologi",
+            "perusahaan" : "Himpunan Mahasiswa Informatika Universitas Sebelas Maret",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/pingtest_it_1.jpg"
+        },
+        {
+            "url_id" : "17", 
+            "judul"   : "Menggali Lebih dalam dunia IT untuk Generasi Z",
+            "perusahaan" : "DiSelenggarakan oleh LP3I Jakarta",
+            "button": "Sertifikat",
+            "url_image": "img/sertifikat/lp3i_it_1.jpg"
+        }
+    
+    ];
+    // akhir sertifikat 3
+
+
+
+// Skill 1
+var skill_1 = [
+    {
+        "nama_skill" : "PHP", 
+        "proses_skill"   : "60%"
+    },
+    {
+        "nama_skill" : "MySql", 
+        "proses_skill"   : "60%"
+    },
+    {
+        "nama_skill" : "HTML", 
+        "proses_skill"   : "70%"
+    },
+    {
+        "nama_skill" : "Figma", 
+        "proses_skill"   : "50%"
+    },
+    {
+        "nama_skill" : "CSS", 
+        "proses_skill"   : "70%"
+    },
+    {
+        "nama_skill" : "Corel Draw", 
+        "proses_skill"   : "50%"
+    },
+    {
+        "nama_skill" : "JavaScript", 
+        "proses_skill"   : "50%"
+    },
+    {
+        "nama_skill" : "Photoshop", 
+        "proses_skill"   : "50%"
+    },
+];
+// AKHIR Skill 1
+
+    
